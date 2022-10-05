161d36f7eac1515dde8f1d0d96d08d833babd193
Alex Gicimo Gakuru <gakurualexgicimo@gmail.com>
2022-10-05 19:14:43 +0300

    Abstracting receipts form

--- src/components/Receipts.js
+++ src/components/Receipts.js
@@ -1,1 +1,26 @@
+import React from "react";
+import Form from "./Form";
 
+function Receipts() {
+
+const receipts={
+    commodityName:"Commodity Name and Description",
+    unitOfIssue:"Unit of issue",
+    itemCode:"Item Code",
+    storageRequirements:"Storage Requirements",
+    averageMonthlyConsumption:"Average Monthly Consumption",
+    minimumValue:"Minimum Level",
+    maximumValue:"Maximum Level",
+
+
+
+
+};
+    return (
+    <React.Fragment>
+
+    </React.Fragment>
+    );
+}
+
+export default Receipts;
