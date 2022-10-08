ae3b98d771b75a37d798547ef0c1fdbaae6c1eec
Alex Gicimo Gakuru <gakurualexgicimo@gmail.com>
2022-10-08 10:54:11 +0300

    Failed to reach objectives due to complexity back to the drawing board

--- src/components/Receipts.js
+++ src/components/Receipts.js
@@ -1,21 +1,9 @@
 import React from "react";
-import Form from "./Form";
+
 
 function Receipts() {
 
-const receipts={
-    commodityName:"Commodity Name and Description",
-    unitOfIssue:"Unit of issue",
-    itemCode:"Item Code",
-    storageRequirements:"Storage Requirements",
-    averageMonthlyConsumption:"Average Monthly Consumption",
-    minimumValue:"Minimum Level",
-    maximumValue:"Maximum Level",
 
-
-
-
-};
     return (
     <React.Fragment>
 
